273b75a7e12003b674c3426cc741be17bcd5325e
Johnathan Beal <johnathanbeal@gmail.com>
2020-08-23 11:19:45 -0400

    small change

--- src/CraigslistScraper/Credentials/EmailAuthenticationInfo.cs
+++ src/CraigslistScraper/Credentials/EmailAuthenticationInfo.cs
@@ -2,8 +2,8 @@ namespace CraigslistScraper.Credentials
 {
     public class EmailAuthenticationInfo
     {
-        public string Username { get { return "AKIAQUTFCN7QWVY5ICM5"; } }
+        public string Username { get { return ""; } }
         
-        public string Password { get { return "BCHEtZOoHyML8W4FxmVvJwjF+sF8yt2Pr8LlwOqIiuOC"; } }
+        public string Password { get { return ""; } }
     }
 }
