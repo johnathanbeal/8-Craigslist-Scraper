22880aef932d14e9c898e680e53d2502e5384d18
Johnathan Beal <johnathanbeal@gmail.com>
2020-08-25 11:24:17 -0400

    rmf

--- src/CraigslistScraper/Credentials/EmailAuthenticationInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace CraigslistScraper.Credentials
-{
-    public class EmailAuthenticationInfo
-    {
-        public string Username { get { return "jbeal.i360@gmail.com"; } }
-        
-        public string Password { get { return ""; } }
-    }
-}
